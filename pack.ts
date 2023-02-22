69f4f7e1f8ff53b8e455509e11deb740239d1658
Roger Zurawicki <zurawiki@users.noreply.github.com>
2023-02-21 19:08:11 -0500

    My first formua

--- pack.ts
+++ pack.ts
@@ -1,3 +1,19 @@
 import * as coda from "@codahq/packs-sdk";
-
 export const pack = coda.newPack();
+import { formatRelative, subDays } from "date-fns";
+
+pack.addFormula({
+  name: "HumanDate",
+  description: "Prints a human-readable date.",
+  parameters: [
+    coda.makeParameter({
+      type: coda.ParameterType.Date,
+      name: "date",
+      description: "The input date.",
+    }),
+  ],
+  resultType: coda.ValueType.String,
+  execute: async function ([date], context) {
+    return formatRelative(date, new Date());
+  },
+});
